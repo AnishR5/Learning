1f5045bdfbcacd0d8f91598fc324830627174e20
Anish <anishrachcha@gmail.com>
2022-09-27 22:08:24 +0530

    New files

---
+++ Python/conditionals_if.py
@@ -0,0 +1,17 @@
+#Conditional expss
+a= int(input("Type 1st number \n"))
+b= int(input("Type 2nd number \n"))
+if(a>b):
+    print("1st number is greater")
+else:
+    print("2nd Number is greater")
+
+
+# logical operators
+c=int(input("Enter your age"))
+d=int(input("Enter your height"))
+
+if(c>18 and d>5.5) :
+    print("You are eligible")
+else :
+    print("You are not eligible") 
